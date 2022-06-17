f34b7a84792a1d4a5cb62a992d1069d11a73cc67
yusupmyratGiHub <ashgabatmekan@gmail.com>
2022-06-17 20:48:44 +0200

    Spring Entry

--- src/main/java/com/tpe/controller/StudentController.java
+++ src/main/java/com/tpe/controller/StudentController.java
@@ -1,35 +1,95 @@
 package com.tpe.controller;
+import java.util.List;
 
-import com.tpe.domain.Student;
 import com.tpe.service.IStudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
-
+import com.tpe.domain.Student;
 @Controller
+@RequestMapping("/students")
 public class StudentController {
 
-
-
-    @RequestMapping("/welcome")
-    @ResponseBody
-    public String welcome(){
-        return "welcome to Student Controller";
-
-    }
     @Autowired
-    private IStudentService iStudentService;
-    @RequestMapping(value="/students",method = RequestMethod.GET)
-    public ModelAndView getStudents(){
+    private IStudentService studentService;
+
+    @GetMapping("/welcome")
+    @ResponseBody
+    public String welcome() {
+        return "Welcome to Student Controller";
+    }
+
+
+    @GetMapping("/sayHi")
+    public ModelAndView sayHi() {
         ModelAndView mav=new ModelAndView();
-        List<Student> list=iStudentService.getAll();
+        mav.addObject("message","Hi");
+        mav.setViewName("try");
+        return mav;
+    }
+
+
+    //@RequestMapping(value="/students", method=RequestMethod.GET)
+    @GetMapping
+    public ModelAndView getStudents() {
+        List<Student> list = studentService.getAll();
+        ModelAndView mav=new ModelAndView();
         mav.addObject("students",list);
         mav.setViewName("students");
         return mav;
     }
+
+
+    @GetMapping("/new")
+    public String newStudent(@ModelAttribute("student") Student student) {
+        return "newStudent";
+    }
+
+
+    @PostMapping("/new")
+    public String createStudent(@ModelAttribute Student student) {
+        studentService.createStudent(student);
+        return "redirect:/students";
+    }
+
+
+    @GetMapping("/update/{id}")
+    public String getStudent(@PathVariable Long id, ModelMap modelMap) {
+        Student student= studentService.findStudent(id);
+        modelMap.put("student", student);
+        return "updateStudent";
+    }
+
+
+    @PostMapping("/update/{id}")
+    public String updateStudent(@ModelAttribute Student student) {
+        studentService.updateStudent(student);
+        return "redirect:/students";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String getStudentForDelete(@PathVariable Long id, ModelMap modelMap) {
+        Student student= studentService.findStudent(id);
+        modelMap.put("student", student);
+        return "deleteStudent";
+    }
+
+
+    @PostMapping("/delete/{id}")
+    public String deleteStudent(@PathVariable Long id) {
+        studentService.deleteStudent(id);
+        return "redirect:/students";
+    }
+
+
+
+
 }
---
+++ src/main/webapp/WEB-INF/jsp/updateStudent.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="ISO-8859-1"%>
+<%@taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="ISO-8859-1">
+    <title>Update Student</title>
+</head>
+
+<body>
+<form:form modelAttribute="student" method="post">
+    First Name:<form:input path="fistName"/><br/>
+    Last Name:<form:input path="lastName"/><br/>
+    Grade:<form:input path="grade"/><br/>
+    <form:button name="submit">Update</form:button>
+</form:form>
+</body>
+</html>
